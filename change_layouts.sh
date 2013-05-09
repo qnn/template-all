30fbe1b7453df3b930f8caa58bcf79a5148e9dc8
caiguanhao <caiguanhao@gmail.com>
2013-05-10 01:39:24 +0800

    fix bash errors on linux;

--- change_layouts.sh
+++ change_layouts.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-LAYOUTS=($(find . -maxdepth 1 -type d \( ! -iname ".*" \) -exec basename \{} \;))
+LAYOUTS=($(find . -maxdepth 1 -type d | sed 's!.*/!!'))
 
 for LAYOUT in "${LAYOUTS[@]}"
 do
 	if [ -f "$LAYOUT/_config.yml" ] && ! grep -q "layouts: _layout_$LAYOUT" "$LAYOUT/_config.yml"
 	then
-		sed -i "" "s/layouts: _layout_.*/layouts: _layout_$LAYOUT/g" "$LAYOUT/_config.yml"
+		sed -i.bak "s/layouts: _layout_.*/layouts: _layout_$LAYOUT/g" "$LAYOUT/_config.yml"
+		rm -f "$LAYOUT/_config.yml.bak"
 		echo "$LAYOUT/_config.yml updated."
 	fi
 done
